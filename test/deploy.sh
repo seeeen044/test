5c7fb051c8291edae45e2a6c5b5581aba38b31ff
seeeen044 <chifuyu1228@gmail.com>
2022-06-16 22:53:22 +0900

    add:new file

---
+++ test/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+########下記、自分のgithubアカウント名と作ったリポジトリ名に置き換えてください########
+git push -f git@github.com:seeen044/test/test.git main:gh-pages
+
+cd -
+
